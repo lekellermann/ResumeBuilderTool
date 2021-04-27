827eb16f5645053206c98673736e460727e9b66e
lekellermann <lekellermann@my.waketech.edu>
2021-04-27 08:07:03 -0400

    Made width adjustment to resume <div> container

--- projectJS.js
+++ projectJS.js
@@ -60,7 +60,6 @@ function generateResume()
     myText +=("<p class= \"info\">LinkedIn Profile: " + linkedIn +"</p><p class=\"info\">Portfolio: " + portfolio + "</p></div>");
     myText += ("</p>\n</div></body>\n</html>");
     
-    myText += ("</div>");
-    myText += ("</body>\n</html>");
+
     resumeWindow.document.write(myText);
 }
